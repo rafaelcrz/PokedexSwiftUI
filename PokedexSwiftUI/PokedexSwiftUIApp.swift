05d049dc6c4de3450622cfb87b4fe2fed31a5f55
Rafael Felipe <rafael.felipe1989@gmail.com>
2020-11-01 21:05:19 -0300

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by rafael ramos on 01/11/20.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
